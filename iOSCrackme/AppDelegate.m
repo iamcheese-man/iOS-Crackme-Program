7cee6057902b53312b1d1d5411f0fea8a6c2c013
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-03-20 00:37:06 +0000

    Update AppDelegate.m

--- iOSCrackme/AppDelegate.m
+++ iOSCrackme/AppDelegate.m
@@ -1,11 +1,17 @@
 #import "AppDelegate.h"
+#import "ViewController.h"
 #import "AntiDebug.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Anti-debug check at launch
     [AntiDebug startWatchdog];
+
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [[ViewController alloc] init];
+    self.window.backgroundColor = [UIColor blackColor];
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
